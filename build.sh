ed6cb253e3dd6919853766693b852b4b489c0eda
piposeimandi <bajosega@gmail.com>
2025-08-13 19:22:28 -0300

    fix version info build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-g++ -std=c++11 -O3 maya22-control.c -o maya22-control -lhidapi-hidraw -lhidapi-libusb
+g++ -std=c++11 -O3 -DVERSION="\"1.0.0\"" maya22-control.c -o maya22-control -lhidapi-hidraw -lhidapi-libusb
 strip maya22-control
